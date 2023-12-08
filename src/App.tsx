bfb481f6c620f50fefbe8eddbb003417aa4c4728
Harsh Mohite <harsh.mohite@fradium.com>
2023-12-08 18:36:08 +0530

    moved intersection logic to custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useRef, useState, useEffect } from "react";
 import "./App.css";
 import Careers from "./components/Careers/Careers";
 import Features from "./components/Features/Features";
@@ -9,46 +8,11 @@ import Team from "./components/Team/Team";
 import Waitlist from "./components/Waitlist/Waitlist";
 import Whitepaper from "./components/Whitepaper/Whitepaper";
 import WaitlistSticky from "./components/Waitlist/WaitlistSticky";
+import useElementVisible from "./components/hooks/useElementVisible";
 
 function App() {
-  const contaierRefOne = useRef<HTMLDivElement>(null);
-  const contaierRefTwo = useRef<HTMLDivElement>(null);
-  const [isVisibleOne, setIsVisibleOne] = useState(false);
-  const [isVisibleTwo, setIsVisibleTwo] = useState(false);
-
-  const callbackOne = (entries: any) => {
-    const [entry] = entries;
-    setIsVisibleOne(entry.isIntersecting);
-  };
-
-  const callbackTwo = (entries: any) => {
-    const [entry] = entries;
-    setIsVisibleTwo(entry.isIntersecting);
-  };
-
-  useEffect(() => {
-    const optionsOne = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.35,
-    };
-
-    const optionsTwo = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.725,
-    };
-
-    const observerOne = new IntersectionObserver(callbackOne, optionsOne);
-    const observerTwo = new IntersectionObserver(callbackTwo, optionsTwo);
-    if (contaierRefOne.current) observerOne.observe(contaierRefOne.current);
-    if (contaierRefTwo.current) observerTwo.observe(contaierRefTwo.current);
-
-    return () => {
-      if (contaierRefOne.current) observerOne.unobserve(contaierRefOne.current);
-      if (contaierRefTwo.current) observerTwo.unobserve(contaierRefTwo.current);
-    };
-  }, [contaierRefOne, contaierRefTwo]);
+  const { contaierRefOne, contaierRefTwo, isVisibleOne, isVisibleTwo } =
+    useElementVisible();
 
   return (
     <div className="relative dynamic-height">
---
+++ src/components/hooks/useElementVisible.tsx
@@ -0,0 +1,46 @@
+import { useRef, useState, useEffect } from "react";
+
+const useElementVisible = () => {
+  const contaierRefOne = useRef<HTMLDivElement>(null);
+  const contaierRefTwo = useRef<HTMLDivElement>(null);
+  const [isVisibleOne, setIsVisibleOne] = useState(false);
+  const [isVisibleTwo, setIsVisibleTwo] = useState(false);
+
+  useEffect(() => {
+    const containerOne = contaierRefOne.current;
+    const containerTwo = contaierRefTwo.current;
+    const observerOne = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        setIsVisibleOne(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.35,
+      }
+    );
+    const observerTwo = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        setIsVisibleTwo(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.725,
+      }
+    );
+    if (containerOne) observerOne.observe(containerOne);
+    if (containerTwo) observerTwo.observe(containerTwo);
+
+    return () => {
+      if (containerOne) observerOne.unobserve(containerOne);
+      if (containerTwo) observerTwo.unobserve(containerTwo);
+    };
+  }, [contaierRefOne, contaierRefTwo]);
+
+  return { contaierRefOne, contaierRefTwo, isVisibleOne, isVisibleTwo };
+};
+
+export default useElementVisible;
