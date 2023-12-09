b35225d5f741ef587ea3b4bfea7251a6226583c6
Harsh Mohite <harsh.mohite@fradium.com>
2023-12-10 04:15:15 +0530

    minor css fix

--- src/components/Careers/Careers.tsx
+++ src/components/Careers/Careers.tsx
@@ -8,9 +8,9 @@ const Careers = () => {
   return (
     <div
       id="careers"
-      className="flex scroll-m-[76px] justify-center bg-primary-green px-5 py-8 lg:scroll-m-[95.2px] lg:p-0"
+      className="flex h-fit scroll-m-[76px] justify-center bg-primary-green px-5 py-8 lg:scroll-m-[95.2px] lg:p-0"
     >
-      <div className="lg:bg-gradient-unmaze-1 rounded-[1.25rem] bg-white px-9 py-12 text-primary-green lg:rounded-none lg:bg-transparent lg:px-[5.625rem] lg:py-16">
+      <div className="lg:bg-gradient-unmaze-1 flex items-center rounded-[1.25rem] bg-white px-9 py-12 text-primary-green lg:rounded-none lg:bg-transparent lg:px-[5.625rem] lg:py-16">
         <div className="flex flex-col gap-4 lg:gap-6">
           <div className="flex flex-col gap-[0.375rem] font-clash-display lg:gap-3 lg:px-4">
             <p className="text-sm font-medium tracking-[0.105rem] lg:text-2xl">
@@ -55,11 +55,11 @@ const Careers = () => {
           </div>
         </div>
       </div>
-      <div className="hidden lg:block lg:w-full lg:overflow-hidden">
+      <div className=" hidden lg:block lg:w-full lg:overflow-hidden">
         <img
           src={officeImage}
           alt="office"
-          className="h-auto w-full -translate-x-[10rem] -translate-y-[3rem] scale-[1.5] object-cover"
+          className="h-full w-full -translate-x-[10rem] -translate-y-[3rem] scale-[1.5] object-cover"
         />
       </div>
     </div>
