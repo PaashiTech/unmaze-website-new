a360da765a726cc843c8daabc1c7ac3ed0a85e22
amoghkulkarnifr <amogh.kulkarni@fradium.com>
2023-12-13 19:13:48 +0530

    Tailwind config + navbar story

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,11 @@ import logo from "../../assets/unmaze-logo-dark.svg";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-unmaze-2 shadow-nav-shadow sticky top-0 z-50 flex w-full items-center justify-center p-5 lg:justify-between lg:px-[5.625rem] lg:py-[1.25rem]">
+    <nav className="sticky top-0 z-50 flex w-full items-center justify-center bg-gradient-unmaze-2 p-5 shadow-nav-shadow lg:justify-between lg:px-[5.625rem] lg:py-[1.25rem]">
       <a href="/">
         <img src={logo} alt="unmaze-logo" />
       </a>
-      <div className="font-font-body hidden items-center gap-20 text-xl tracking-wider text-primary-green lg:flex">
+      <div className="hidden items-center gap-20 font-font-body text-xl tracking-wider text-primary-green lg:flex">
         <a href="#resources">Resources</a>
         <a href="#team">Team</a>
         <a href="#careers">Careers</a>
