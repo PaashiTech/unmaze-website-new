ab5dd314f5a9dbd32bef32326b814861024b15fb
Harsh Mohite <harsh.mohite@fradium.com>
2023-12-19 22:51:42 +0530

    UNMZ-72 style: unmaze watermark

--- src/components/_UI/BackgroundLogo.tsx
+++ src/components/_UI/BackgroundLogo.tsx
@@ -1,7 +1,7 @@
 import backgroundLogo from "../../assets/background-logo.svg";
 const BackgroundLogo = () => {
   return (
-    <div className="absolute right-[-12rem] top-[3rem] w-[31rem] rotate-[80deg] lg:right-[-3rem] lg:top-[-3rem] lg:w-[37.5rem] lg:rotate-0">
+    <div className="absolute right-[-12rem] top-[3rem] w-[31rem] lg:left-0 lg:right-0 lg:mx-auto lg:w-[37.5rem]">
       <img
         src={backgroundLogo}
         alt="background-logo"
