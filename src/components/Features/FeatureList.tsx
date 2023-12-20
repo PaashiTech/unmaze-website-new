ac61268362c95789c1fc09651ebcfe61f49fce74
Harsh Mohite <harsh.mohite@fradium.com>
2023-12-20 17:17:38 +0530

    style: added desktop svgs for feature list

--- src/components/Features/FeatureList.tsx
+++ src/components/Features/FeatureList.tsx
@@ -3,14 +3,19 @@ import { featuresData } from "./featuresData";
 import Text from "../_UI/Typography/Text";
 
 type FeatureProps = {
-  icon: string;
+  iconMobile: string;
+  iconDesktop: string;
   text: string;
 };
 
-const Feature: React.FC<FeatureProps> = ({ icon, text }) => {
+const Feature: React.FC<FeatureProps> = ({ iconMobile, iconDesktop, text }) => {
   return (
     <div className="flex gap-5 px-4">
-      <img src={icon} alt="feature-icon" />
+      <picture>
+        <source srcSet={iconDesktop} media="(min-width: 1024px)" />
+        <img src={iconMobile} alt="feature-icon" />
+      </picture>
+
       <Text size="xl">{text}</Text>
     </div>
   );
@@ -20,7 +25,12 @@ const FeatureList = () => {
   return (
     <div className="flex flex-col gap-8 text-[#2a2a2a]">
       {featuresData.map((feature) => (
-        <Feature key={feature.id} icon={feature.icon} text={feature.text} />
+        <Feature
+          key={feature.id}
+          iconMobile={feature.iconMobile}
+          iconDesktop={feature.iconDesktop}
+          text={feature.text}
+        />
       ))}
     </div>
   );
