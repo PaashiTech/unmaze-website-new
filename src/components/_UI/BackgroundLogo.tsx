b46bfc62f5d1c0ab2d53ee93b2ee16f491f6af81
Harsh Mohite <harsh.mohite@fradium.com>
2023-12-20 14:02:38 +0530

    style: fixed background logo position

--- src/components/_UI/BackgroundLogo.tsx
+++ src/components/_UI/BackgroundLogo.tsx
@@ -1,7 +1,7 @@
 import backgroundLogo from "../../assets/background-logo.svg";
 const BackgroundLogo = () => {
   return (
-    <div className="absolute right-[-12rem] top-[3rem] w-[31rem] lg:left-0 lg:right-0 lg:mx-auto lg:w-[37.5rem]">
+    <div className="absolute right-[-12rem] top-[3rem] w-[31rem] lg:right-[-3rem] lg:w-[37.5rem]">
       <img
         src={backgroundLogo}
         alt="background-logo"
